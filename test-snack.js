b449c078fceef64a1ee00250a9ba17f1f9518d90
Ivanaricci <ivana.ricci26@gmail.com>
2025-10-20 17:26:12 +0200

    [UPD] SNACK2

--- test-snack.js
+++ test-snack.js
@@ -5,7 +5,13 @@ function getInitials(fullName) {
     return fullName.split(' ').map(word => word[0].toUpperCase()).join('')
 }
 
+// SNACK2
+function createSlug(name) {
+    return name.toLowerCase()
+}
+
 
 module.exports = {
-    getInitials
+    getInitials,
+    createSlug
 }
--- test-snack.test.js
+++ test-snack.test.js
@@ -1,4 +1,4 @@
-const { getInitials } = require("./test-snack")
+const { getInitials, createSlug } = require("./test-snack")
 
 
 
@@ -7,7 +7,18 @@ const { getInitials } = require("./test-snack")
 // Creare un test che verifichi la seguente descrizione:
 // 👉 "La funzione getInitials restituisce le iniziali di un nome completo."
 
-
 test('La funzione getInitials restituisce le iniziali di un nome completo', () => {
     expect(getInitials('Ivana Ricci')).toBe('IR')
+})
+
+
+
+
+// SNACK2
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione createSlug restituisce una stringa in lowercase."
+
+
+test('La funzione createSlug restituisce una stringa in lowercase', () => {
+    expect(createSlug('IVANA')).toBe('ivana')
 })
